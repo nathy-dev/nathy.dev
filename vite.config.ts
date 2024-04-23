93ed32e58db2c9d916e2f430fa5fc2251e70558f
nathy <njmackinlay@gmail.com>
2024-04-22 22:34:43 -0700

    Use esbuild instead of terser.

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   esbuild: {
-    drop: ['console', 'debugger'],
-  },
-  build: {
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
+    drop: ['console'],
   },
 });
