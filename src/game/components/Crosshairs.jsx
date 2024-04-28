3a61c7b9055e2422721e2e885908115404524f5c
nathy <njmackinlay@gmail.com>
2024-04-28 10:41:36 -0700

    Update crosshairs to better track camera movement.

--- src/game/components/Crosshairs.jsx
+++ src/game/components/Crosshairs.jsx
@@ -7,14 +7,17 @@ export const Crosshairs = () => {
   const { camera } = useThree();
 
   useFrame(() => {
+    camera.updateProjectionMatrix();
+    camera.updateWorldMatrix();
     const vector = new Vector3(0, 0, -0.8).unproject(camera);
     dot.current.position.set(...vector.toArray());
+    dot.current.lookAt(camera.position);
   });
 
   return (
     <mesh ref={dot}>
-      <sphereGeometry args={[0.002, 64, 32]} />
-      <meshBasicMaterial color={'purple'} />
+      <ringGeometry args={[0.002, 0.003, 16]} />
+      <meshBasicMaterial color={0xbe7bf9} />
     </mesh>
   );
 };
