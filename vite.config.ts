9ca43843de419beed8175f9d3a4360f37676e3a9
nathy <njmackinlay@gmail.com>
2024-04-30 06:11:21 -0700

    Remove unneeded plugin.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import removeConsole from 'vite-plugin-remove-console';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), removeConsole()],
+  plugins: [react()],
   esbuild: {
     drop: ['console'],
   },
