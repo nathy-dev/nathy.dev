ce6b9cb82db8cbf011cb7764117cbd69c020be2f
nathy <njmackinlay@gmail.com>
2024-05-19 12:57:15 -0700

    Add mobile fallback.

--- src/game/Game.jsx
+++ src/game/Game.jsx
@@ -15,6 +15,9 @@ import { VictoryOverlay } from './components/VictoryOverlay.tsx';
 import { DeathOverlay } from './components/DeathOverlay.tsx';
 
 import { useRoute } from 'wouter';
+import { useDetectMobile } from '../hooks/useDetectMobile.ts';
+import { PixelBox } from '../components/PixelBox.tsx';
+import { Choice } from '../components/Choice.tsx';
 
 const LazyAudio = lazy(() => import('./components/Audio.tsx'));
 
@@ -23,6 +26,7 @@ const Game = () => {
   const { toggleMute, status, resetState } = useGameStore();
   const balladRef = useRef(null);
   const FPVref = useRef(null);
+  const isMobile = useDetectMobile();
 
   const [match] = useRoute('/resume-rummager');
 
@@ -41,28 +45,53 @@ const Game = () => {
 
   return (
     <div className="absolute h-full w-full overflow-hidden">
-      <TitleScreen controlRef={FPVref} />
-      <UI>
-        <Staff />
-        <PlayerUi />
-        <MuteButton />
-        <VictoryOverlay controlRef={FPVref} />
-        <DeathOverlay controlRef={FPVref} />
-      </UI>
-      <Canvas
-        shadows={{
-          type: 'BasicShadowMap',
-        }}
-        mode="concurrent"
-        camera={{ position: [0, 5, 0], rotation: [0, 3.2, 0] }}
-      >
-        <Map />
-        <FPVControls controlRef={FPVref} />
-      </Canvas>
-      {status !== 'init' && (
-        <Suspense fallback={null}>
-          <LazyAudio volume={1} loop={true} sound={Sound.ballad} audioRef={balladRef} />
-        </Suspense>
+      {isMobile ? (
+        <div className="absolute bottom-0 left-0 right-0 top-0 m-auto h-fit w-fit">
+          <PixelBox>
+            <div className="flex flex-col items-center gap-8">
+              <h1>Mobile game coming soon</h1>
+              <div>
+                <p>To play, visit again on a desktop.</p>
+                <p>Here is the reward for free.</p>
+              </div>
+
+              <Choice
+                icon="chest"
+                handleChoiceClick={() => {
+                  window.open('resume.pdf', '_blank');
+                }}
+              >
+                Open treasure?
+              </Choice>
+            </div>
+          </PixelBox>
+        </div>
+      ) : (
+        <>
+          <TitleScreen controlRef={FPVref} />
+          <UI>
+            <Staff />
+            <PlayerUi />
+            <MuteButton />
+            <VictoryOverlay controlRef={FPVref} />
+            <DeathOverlay controlRef={FPVref} />
+          </UI>
+          <Canvas
+            shadows={{
+              type: 'BasicShadowMap',
+            }}
+            mode="concurrent"
+            camera={{ position: [0, 5, 0], rotation: [0, 3.2, 0] }}
+          >
+            <Map />
+            <FPVControls controlRef={FPVref} />
+          </Canvas>
+          {status !== 'init' && (
+            <Suspense fallback={null}>
+              <LazyAudio volume={1} loop={true} sound={Sound.ballad} audioRef={balladRef} />
+            </Suspense>
+          )}
+        </>
       )}
     </div>
   );
